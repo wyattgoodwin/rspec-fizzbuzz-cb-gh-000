89aba665cb4c5c5b25028a1197bece2595c28224
Wyatt Goodwin <wog6@cornell.edu>
2019-09-05 18:58:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(X)
+def fizzbuzz(x)
   if x/5 == 0 && x/3 == 0
     puts "fizzbuzz"
   elseif x/5 == 0
@@ -9,4 +9,3 @@ def fizzbuzz(X)
     puts "fizz"
   end
 end
- 
